63f6d0ddc2fd9f691aeaf01c28832bff9e76d065
anague sonna roosvelt <anaguesonnaroosvelt@gmail.com>
2023-11-14 18:45:06 +0100

    make the hero part of the frontend dynamic

--- app/Livewire/WebFrontend/Ui/HeroComponent.php
+++ app/Livewire/WebFrontend/Ui/HeroComponent.php
@@ -2,17 +2,36 @@
 
 namespace App\Livewire\WebFrontend\Ui;
 
+use App\Models\Hero;
 use Livewire\Component;
+use Illuminate\Support\Facades\DB;
 
 class HeroComponent extends Component
 {
     public function render()
     {
+
+        // $hero = Hero::where("status","active")->first();
+
         return <<<'blade'
-            <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+            @php
+
+            $hero = DB::table('heroes')->where("status","active")->first();
+
+            @endphp
+            <section id="hero" class="d-flex flex-column justify-content-center align-items-center" >
+                <style>
+                #hero {
+                    width: 100%;
+                    height: 100vh;
+                    background: url("assets/img/{{$hero->image}}") top center;
+                    background-size: cover;
+                  }
+
+                </style>
                 <div class="hero-container" data-aos="fade-in">
-                <h1>Alex Smith</h1>
-                <p>I'm <span class="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span></p>
+                <h1>{{$hero->title}}</h1>
+                <p>I'm <span class="typed" data-typed-items="{{$hero->skill}}"></span></p>
                 </div>
             </section>
         blade;
